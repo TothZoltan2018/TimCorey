2c63bd10f73e3909d5b8cb841200a959e7a8d7a0
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-05-11 22:04:29 +0200

    DRY - Start state as get from Tim in email

---
+++ DRY/WinFormUI/Dashboard.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormUI
+{
+    public partial class Dashboard : Form
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void generateEmployeeIdButton_Click(object sender, EventArgs e)
+        {
+            string employeeId = $@"{ firstNameText.Text.Substring(0, 4) }{ lastNameText.Text.Substring(0, 4) }{ DateTime.Now.Millisecond }";
+            employeeIdText.Text = employeeId;
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,6 @@
+This repo contains some solutions from Timm Corey's learning path:
+https://www.youtube.com/user/IAmTimCorey/playlists?view=50&sort=dd&shelf_id=5
+
+Folder DRY
+Design Patterns: Don't Repeat Yourself in C#
+https://www.youtube.com/watch?v=dhnsegiPXoo&list=PLLWMQd6PeGY2GVsQZ-u3DPXqwwKW8MkiP&index=10
