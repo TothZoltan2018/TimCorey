989692d0ae2dd662c5c200fd0e4458815739bd3a
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-06-17 23:10:14 +0200

    DelegatesDemo  - A Func<> is added

--- DelegatesDemo/ConsoleUI/Program.cs
+++ DelegatesDemo/ConsoleUI/Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleUI
         {
             PopulateCartWithDemoData();
 
-            Console.WriteLine($"The total for the cart is {cart.GenerateTotal(SubTotalAlert):C2}");
+            Console.WriteLine($"The total for the cart is {cart.GenerateTotal(SubTotalAlert, CalculateLeveledDiscount):C2}");
             
 
             Console.WriteLine();
@@ -26,6 +26,26 @@ namespace ConsoleUI
         private static void SubTotalAlert(decimal subTotal)
         {
             Console.WriteLine($"The subtotal is {subTotal:C2}");
+        }
+
+        private static decimal CalculateLeveledDiscount(List<ProductModel> items, decimal subTotal)
+        {
+            if (subTotal > 100)
+            {
+                return subTotal * 0.80M;
+            }
+            if (subTotal > 50)
+            {
+                return subTotal * 0.85M;
+            }
+            if (subTotal > 10)
+            {
+                return subTotal * 0.95M;
+            }
+            else
+            {
+                return subTotal;
+            }
         }
         private static void PopulateCartWithDemoData()
         {
--- DelegatesDemo/DemoLibrary/ShoppingCartModel.cs
+++ DelegatesDemo/DemoLibrary/ShoppingCartModel.cs
@@ -11,28 +11,14 @@ namespace DemoLibrary
         public delegate void MentionDiscount(decimal subTotalThisNameCanBeAnything);
         public List<ProductModel> Items { get; set; } = new List<ProductModel>();
         
-        public decimal GenerateTotal(MentionDiscount mentionDiscount)
+        public decimal GenerateTotal(MentionDiscount mentionDiscount,
+            Func<List<ProductModel>, decimal, decimal> calculateDiscountedTotal)
         {
             decimal subTotal = Items.Sum(x => x.Price);
 
             mentionDiscount(subTotal);
 
-            if (subTotal > 100)
-            {
-                return subTotal * 0.80M;
-            }
-            if (subTotal > 50)
-            {
-                return subTotal * 0.85M;
-            }
-            if (subTotal > 10)
-            {
-                return subTotal * 0.90M;
-            }
-            else
-            {
-                return subTotal;
-            }            
+            return calculateDiscountedTotal(Items, subTotal);
         }
     }
 }
