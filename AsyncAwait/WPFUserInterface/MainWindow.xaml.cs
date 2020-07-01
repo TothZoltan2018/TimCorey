4532ae2a4812f75c5cd420cbdc2b2ce911666f17
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-07-01 07:20:47 +0200

    SimpleAsyncDemo

--- AsyncAwait/WPFUserInterface/MainWindow.xaml.cs
+++ AsyncAwait/WPFUserInterface/MainWindow.xaml.cs
@@ -29,10 +29,14 @@ namespace WPFUserInterface
             resultsWindow.Text += $"Total execution time: { elapsedMs }";
         }
 
+        // 'async' whatever called this method (in this case the UI), that can go on before it finishes.
         private async void executeAsync_Click(object sender, RoutedEventArgs e)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
 
+            // Execution pauses and waits for returning the methods below
+            // Without 'await' the Stopwatch would show stupid low values.
+            // await RunDownloadAsync(); 
             await RunDownloadParallelAsync();
 
             watch.Stop();
@@ -57,12 +61,27 @@ namespace WPFUserInterface
             return output;
         }
 
+        /// <summary>
+        /// If we don't have control over the synchronous method 'DownloadWebsite'
+        /// but we still need to run it asyncronously then use Task.Run.
+        /// Tip: Don't return void from an async method. If there's nothing to return, use 'Task'
+        /// Or if there's a return type e.g. string, use Task<string>
+        /// One exception: In case of event leave the return type void. 
+        /// Naming convention: Append 'Async' to the end of the methodname which going to return asynchronously.
+        /// 'async' means the caller of this method can go on with their task!
+        ///
+        /// </summary>
+        /// <returns></returns>
         private async Task RunDownloadAsync()
         {
             List<string> websites = PrepData();
 
             foreach (string site in websites)
             {
+                //Starts DownloadWebsite method and waits it to return
+                //Loads sites one by one
+                //This cause similar execution time as synchronous RunDownloadSync()
+                //But at least the UI will be responsive
                 WebsiteDataModel results = await Task.Run(() => DownloadWebsite(site));
                 ReportWebsiteInfo(results);
             }
@@ -75,9 +94,12 @@ namespace WPFUserInterface
 
             foreach (string site in websites)
             {
+                // We put all tasks to a List. The tasks start to run
+                //tasks.Add(Task.Run(() => DownloadWebsite(site)));
                 tasks.Add(DownloadWebsiteAsync(site));
             }
 
+            // waits until all tasks on the list aer done
             var results = await Task.WhenAll(tasks);
 
             foreach (var item in results)
@@ -104,7 +126,7 @@ namespace WPFUserInterface
 
             output.WebsiteUrl = websiteURL;
             output.WebsiteData = client.DownloadString(websiteURL);
-
+            
             return output;
         }
 
