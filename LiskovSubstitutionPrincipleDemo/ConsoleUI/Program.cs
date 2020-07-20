10f657fab830fec7e0b10fd92c84ff6f9dc6e0e3
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-07-20 07:45:09 +0200

    Design Patterns: Liskov Substitution Principle Start state with comments on LSP and the problems with the current situation

--- LiskovSubstitutionPrincipleDemo/ConsoleUI/Program.cs
+++ LiskovSubstitutionPrincipleDemo/ConsoleUI/Program.cs
@@ -17,11 +17,25 @@ namespace ConsoleUI
             accountingVP.LastName = "Stone";
             accountingVP.CalculatePerHourRate(4);
 
-            Employee emp = new CEO();
+            //Employee emp = new Employee(); //LSP says, new Manager() can be used instead of new Employee()
+            // Of course the CalaculatePerHourRate() method will work differently, but the Manager cannot
+            // Throw exception where the Employee would not have.
+            // Employee emp = new Manager(); // This works fine
+
+            // Covariants: return type of a method cannot change. 
+            // Contravariants Cannot change the parameters type.
+
+            // Also, cannot strengthen preconditions in the child class. Eg. Introduce an input parameter check in a method of the Manager class
+            // Also, cannot Weaken postconditions in the child class. Eg. a method of the Manager class cannot return values in wider range than
+            // the method of the Employee class (if it would have limited).
+
+            // So actually a Manager is not really an Employee, because of not having Manager property and method. Therefore inheritance is not the best way.
+
+            Employee emp = new CEO(); // This throws exception
 
             emp.FirstName = "Tim";
             emp.LastName = "Corey";
-            emp.AssignManager(accountingVP);
+            emp.AssignManager(accountingVP); // Throwa exception for CEO 
             emp.CalculatePerHourRate(2);
 
             Console.WriteLine($"{ emp.FirstName }'s salary is ${ emp.Salary }/hour.");
