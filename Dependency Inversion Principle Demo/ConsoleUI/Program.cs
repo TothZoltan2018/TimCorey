2ad7040b78779004386605c2adf2270f3ea2a4af
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-07-21 16:42:36 +0200

    Design Patterns: Dependency Inversion start state.

---
+++ Dependency Inversion Principle Demo/ConsoleUI/Program.cs
@@ -0,0 +1,42 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        /// <summary>
+        /// DI: High level modules (the ones call something else) should not depend on low level (the called) modules. 
+        /// Rather both should depend on abstractions and those abstractions should not depend on details.
+        /// This needs to be inverted.
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Person owner = new Person
+            {
+                FirstName = "Tim",
+                LastName = "Corey",
+                EmailAddress = "tim@iamtimcorey.com",
+                PhoneNumber = "555-1212"
+            };
+
+            Chore chore = new Chore
+            {
+                ChoreName = "Take out the trash",
+                Owner = owner
+            };
+
+            chore.PerformedWork(3);
+            chore.PerformedWork(1.5);
+            chore.CompleteChore();
+
+            Console.ReadLine();
+        }
+    }
+}
