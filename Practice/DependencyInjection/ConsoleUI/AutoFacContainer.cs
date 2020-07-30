61f89be6dba0fe2075ca677e126c00220e2a17c8
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-07-30 23:03:56 +0200

    Practicing Dependency Injection with AutoFac : AutoFac nuget package installed, the container class is added for registration of classes.

---
+++ Practice/DependencyInjection/ConsoleUI/AutoFacContainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    class AutoFacContainer
+    {
+        public static IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Emailer>().As<IMessageSender>();
+
+            //This goes over the DemoLibrary and registers all classes except the Emailer.
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(DemoLibrary)))
+                .Where(t => t.Name.Contains(nameof(Emailer)) == false)
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));
+            
+            return builder.Build();
+        }
+    }
+}
--- Practice/DependencyInjection/DemoLibrary/Chore.cs
+++ Practice/DependencyInjection/DemoLibrary/Chore.cs
@@ -8,27 +8,32 @@ namespace DemoLibrary
 {
     public class Chore
     {
+        private ILogger _logger;
+        private IMessageSender _messageSender;
+
         public string ChoreName { get; set; }
         public Person Owner { get; set; }
         public double HoursWorked { get; private set; }
         public bool IsComplete { get; private set; }
+        public Chore(ILogger logger, IMessageSender messageSender)
+        {
+            _logger = logger;
+            _messageSender = messageSender;
+        }
 
         public void PerformedWork(double hours)
         {
             HoursWorked += hours;
-            Logger log = new Logger();
-            log.Log($"Performed work on { ChoreName }");
+            _logger.Log($"Performed work on { ChoreName }");
         }
 
         public void CompleteChore()
         {
             IsComplete = true;
-
-            Logger log = new Logger();
-            log.Log($"Completed { ChoreName }");
-
-            Emailer emailer = new Emailer();
-            emailer.SendEmail(Owner, $"The chore { ChoreName } is complete.");
+                       
+            _logger.Log($"Completed { ChoreName }");
+            
+            _messageSender.SendEmail(Owner, $"The chore { ChoreName } is complete.");
         }
     }
 }
