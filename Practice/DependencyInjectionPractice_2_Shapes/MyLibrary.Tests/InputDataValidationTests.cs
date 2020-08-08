595cd878c83394e264a8c0a31eb1d8485968f7f6
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-08-08 08:46:39 +0200

    Dependency Injection practice: Area and perimenter calculator for shapes. Unit test of input validation with Xunit added. Shape Area and Perimeter calculation cannot be tested, these classes needs to be reworked.

---
+++ Practice/DependencyInjectionPractice_2_Shapes/MyLibrary.Tests/InputDataValidationTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using MyLibrary.Utilities;
+
+namespace MyLibrary.Tests
+{
+    public class InputDataValidationTests
+    {
+        [Theory]
+        [InlineData("1")]
+        [InlineData("1.0")]
+        [InlineData("2222")]
+        public void Validate_StringShouldBeParsed(string x)
+        {
+            //Arrange
+
+            //Act
+            MyValidator myValidator = new MyValidator(null);
+            var actual = myValidator.Validate(x);
+
+            //Assert
+            Assert.True(actual.IsValid);            
+        }
+
+        [Theory]
+        [InlineData("-1")]
+        [InlineData("0")]
+        [InlineData("0.0")]
+        [InlineData("111111111111111111111111111111111111111111111111111111")]
+        [InlineData("0ssdf")]
+        [InlineData("x")]
+        public void Validate_StringShouldFail(string x)
+        {
+            //Arrange
+
+            //Act
+            MyValidator myValidator = new MyValidator(null);
+            var actual = myValidator.Validate(x);
+    
+            //Assert
+            Assert.False(actual.IsValid);
+        }
+
+
+    }
+}
---
+++ Practice/DependencyInjectionPractice_2_Shapes/MyLibrary.Tests/Shapes.Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Xunit;
+using MyLibrary.Shapes;
+
+namespace MyLibrary.Tests
+{
+    // The Shape classes cannot be tested. Needs to be reworked so that
+    // they not write results out but return value.
+    public class Shapes
+    {        
+        [Theory]
+        [InlineData(3, 4, 12)]
+        public void RectangleArea(double sideA, double sideB, double Area)
+        {
+            Rectangle rectangle = new Rectangle(null, null);
+
+            rectangle.CalculateArea();
+            
+        }
+    }
+}
