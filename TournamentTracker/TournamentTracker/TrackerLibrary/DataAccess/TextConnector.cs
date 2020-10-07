4c550cb9cda6ba03ba4ad10ff03b1f72b88067fb
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-10-07 10:30:21 +0200

    Tornament Tracker - Lesson 11 - Text Connection. Adding some extension methods.

--- TournamentTracker/TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
+++ TournamentTracker/TournamentTracker/TrackerLibrary/DataAccess/TextConnector.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrackerLibrary.DataAccess.TextHelpers;
 using TrackerLibrary.Models;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
         // TODO - Make the CreatePrize method actually save to a textfile
         /// <summary>
         /// Saves a new prize to a textfile
@@ -17,7 +19,24 @@ namespace TrackerLibrary.DataAccess
         /// <returns>The prize information, including the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // It uses our string extension methods
+            // Load the text file and convert the text to List<PizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // Find the max ID
+            int currentId = 1;
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+            
+            model.Id = currentId;
+
+            // Add the new revord with the new id (max +1)
+            prizes.Add(model);
+
+            // Convert the prizes to List<string> Save the list<string> to the text file
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
