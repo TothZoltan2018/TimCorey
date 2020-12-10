08d155917edf96fdc8156bd9bff0fc3f1ec0ff1c
toth.tozso.zoltan <toth.tozso.zoltan@gmail.com>
2020-12-10 19:46:35 +0100

    Intro to WebAPI - Helper page documentation, publishing

--- WbAPIDemoApp/DemoAPI/Controllers/PeopleController.cs
+++ WbAPIDemoApp/DemoAPI/Controllers/PeopleController.cs
@@ -8,6 +8,13 @@ using System.Web.Http;
 
 namespace DemoAPI.Controllers
 {
+    /// <summary>
+    /// This is where I give all the information about my peeps. Zoli: Ez egy sajat dokumentacio,
+    /// 1. Properties --> Build --> Xml documetation file.
+    /// 2. Areas/HelpPage/App_Start/HelpPageConfig: Uncomment: 
+    /// config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/Documentation.xml")));
+    /// </summary>
+        
     public class PeopleController : ApiController
     {
         List<Person> people = new List<Person>();
@@ -17,6 +24,27 @@ namespace DemoAPI.Controllers
             people.Add(new Person { FirstName = "Zoli", LastName = "Toth", Id = 1 });
             people.Add(new Person { FirstName = "Darth", LastName = "Vader", Id = 2 });
             people.Add(new Person { FirstName = "Luke", LastName = "Skywalker", Id = 3 });
+        }
+
+        // ### this xml comment will be shown in the browser ###
+        /// <summary>
+        /// Gets a list of the firstnames of all users
+        /// </summary>
+        /// <param name="userId">The unique identifier for this person.</param>
+        /// <param name="age">We want to know how old they are.</param>
+        /// <returns>A list of firstnames...duh</returns>
+        [Route("api/People/GetFN/{userId:int}/{age:int}")] // Ezzel hivhato a bongeszobol. Tetszoleges utvonalstring adhato
+        [HttpGet]
+        public List<string> GetFirstNames(int userId, int age)
+        {
+            List<string> output = new List<string>();
+
+            foreach (var p in people)
+            {
+                output.Add(p.FirstName);
+            }
+
+            return output;
         }
 
         // GET: api/People // kimegy az az applikacionkbol
@@ -52,5 +80,3 @@ namespace DemoAPI.Controllers
         }
     }
 }
-
-53:45
--- WbAPIDemoApp/DemoAPI/Models/Person.cs
+++ WbAPIDemoApp/DemoAPI/Models/Person.cs
@@ -5,10 +5,22 @@ using System.Web;
 
 namespace DemoAPI.Models
 {
+    /// <summary>
+    /// Represents one specific person
+    /// </summary>
     public class Person
     {
+        /// <summary>
+        /// ID from SQL
+        /// </summary>
         public int Id { get; set; } = 0;
+        /// <summary>
+        /// The user's first name
+        /// </summary>
         public string FirstName { get; set; } = "";
+        /// <summary>
+        /// Couldn't you guess?
+        /// </summary>
         public string LastName { get; set; } = "";
     }
 }
